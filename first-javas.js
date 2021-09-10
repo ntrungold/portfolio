0fc74f95b5f6a7fbea2c6b3f3749afbd6392962d
ntrungold <89802785+ntrungold@users.noreply.github.com>
2021-09-09 20:37:47 -0400

    Add files via upload

---
+++ first-javas.js
@@ -0,0 +1,4 @@
+var firstname = "Noah";
+var lastname = "Trungold";
+var email = "trungold@udel.edu";
+var fullname = firstname + " " + lastname;
---
+++ hello-world.ts
@@ -0,0 +1,10 @@
+console.log('Welcome back to the website!');
+
+interface Creator {
+    name: string;
+    email: string;
+}
+const creator:Creator = {
+    name: "Noah Trungold",
+    email: "trungold@udel.edu"
+}
