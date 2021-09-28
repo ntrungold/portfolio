420e2b368889fe314cedd8789ea96e08e8c1f5b5
Noah Trungold <trungold@udel.edu>
2021-09-28 10:27:55 -0400

    adding in all the buttons to the gameboard, making it conditional of a click

--- new-app/src/components/GameBoard.tsx
+++ new-app/src/components/GameBoard.tsx
@@ -5,20 +5,20 @@ export function GameBoard({set, answerRevealed, reveal}:
         return <Table bordered>
             <tbody>
                 <tr>
-                    <td><Button onClick={() => reveal(!answerRevealed)}>{answerRevealed && set.a1}</Button></td>
-                    <td>5</td>
+                    <td><Button onClick={() => reveal(!answerRevealed)}>{answerRevealed ? 1: set.a1}</Button></td>
+                    <td><Button onClick={() => reveal(!answerRevealed)}>{answerRevealed ? 5: set.a5}</Button></td>
                 </tr>
                 <tr>
-                    <td>2</td>
-                    <td>6</td>
+                    <td><Button onClick={() => reveal(!answerRevealed)}>{answerRevealed ? 2: set.a2}</Button></td>
+                    <td><Button onClick={() => reveal(!answerRevealed)}>{answerRevealed ? 6: set.a6}</Button></td>
                 </tr>
                 <tr>
-                    <td>3</td>
-                    <td>7</td>
+                    <td><Button onClick={() => reveal(!answerRevealed)}>{answerRevealed ? 3: set.a3}</Button></td>
+                    <td><Button onClick={() => reveal(!answerRevealed)}>{answerRevealed ? 7: set.a7}</Button></td>
                 </tr>
                 <tr>
-                    <td>4</td>
-                    <td>8</td>
+                    <td><Button onClick={() => reveal(!answerRevealed)}>{answerRevealed ? 4: set.a4}</Button></td>
+                    <td><Button onClick={() => reveal(!answerRevealed)}>{answerRevealed ? 8: set.a8}</Button></td>
                 </tr>
             </tbody>
         </Table>
