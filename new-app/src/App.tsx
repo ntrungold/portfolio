4a575e96966b8d2b6834dd52ed796460ed3b646c
Noah Trungold <trungold@udel.edu>
2021-09-30 19:23:54 -0400

    fixing error that linked all gameboard buttons as one

--- new-app/src/App.tsx
+++ new-app/src/App.tsx
@@ -11,8 +11,15 @@ import {Set} from './interfaces/set';
 
 function App(): JSX.Element {
   const [activeSet, setActiveSet] = useState<Set>(SETS[0] as Set);
-  const [answerRevealed, reveal] = useState<boolean>(false);
-  
+  const [answerRevealed1, reveal1] = useState<boolean>(false);
+  const [answerRevealed2, reveal2] = useState<boolean>(false);
+  const [answerRevealed3, reveal3] = useState<boolean>(false);
+  const [answerRevealed4, reveal4] = useState<boolean>(false);
+  const [answerRevealed5, reveal5] = useState<boolean>(false);
+  const [answerRevealed6, reveal6] = useState<boolean>(false);
+  const [answerRevealed7, reveal7] = useState<boolean>(false);
+  const [answerRevealed8, reveal8] = useState<boolean>(false);
+
   return (
     <Container className="App">
       <Row>
@@ -23,7 +30,19 @@ function App(): JSX.Element {
         <SetViewer set={activeSet}></SetViewer>
       </Row>
       <Row>
-        <GameBoard set={activeSet} reveal={reveal} answerRevealed= {answerRevealed}></GameBoard>
+        <GameBoard 
+          set={activeSet} 
+          reveal1={reveal1} answerRevealed1= {answerRevealed1}
+          reveal2={reveal2} answerRevealed2= {answerRevealed2}
+          reveal3={reveal3} answerRevealed3= {answerRevealed3}
+          reveal4={reveal4} answerRevealed4= {answerRevealed4}
+          reveal5={reveal5} answerRevealed5= {answerRevealed5}
+          reveal6={reveal6} answerRevealed6= {answerRevealed6}
+          reveal7={reveal7} answerRevealed7= {answerRevealed7}
+          reveal8={reveal8} answerRevealed8= {answerRevealed8}
+          
+          
+          ></GameBoard>
       </Row>
         
     </Container>
--- new-app/src/components/GameBoard.tsx
+++ new-app/src/components/GameBoard.tsx
@@ -1,24 +1,42 @@
 import {Table, Button} from 'react-bootstrap'
 import {Set} from '../interfaces/set'
-export function GameBoard({set, answerRevealed, reveal}: 
-    {set: Set,answerRevealed: boolean, reveal: (r:boolean) =>void}): JSX.Element {
+export function GameBoard({set, 
+    answerRevealed1, reveal1,
+    answerRevealed2, reveal2,
+    answerRevealed3, reveal3,
+    answerRevealed4, reveal4,
+    answerRevealed5, reveal5,
+    answerRevealed6, reveal6,
+    answerRevealed7, reveal7,
+    answerRevealed8, reveal8,
+}: 
+    {set: Set,
+        answerRevealed1: boolean,  reveal1: (r:boolean) =>void,
+        answerRevealed2: boolean,  reveal2: (r:boolean) =>void,
+        answerRevealed3: boolean,  reveal3: (r:boolean) =>void,
+        answerRevealed4: boolean,  reveal4: (r:boolean) =>void,
+        answerRevealed5: boolean,  reveal5: (r:boolean) =>void,
+        answerRevealed6: boolean,  reveal6: (r:boolean) =>void,
+        answerRevealed7: boolean,  reveal7: (r:boolean) =>void,
+        answerRevealed8: boolean,  reveal8: (r:boolean) =>void
+    }): JSX.Element {
         return <Table bordered>
             <tbody>
                 <tr>
-                    <td><Button onClick={() => reveal(!answerRevealed)}>{answerRevealed ? 1: set.a1}</Button></td>
-                    <td><Button onClick={() => reveal(!answerRevealed)}>{answerRevealed ? 5: set.a5}</Button></td>
+                    <td><Button onClick={() => reveal1(!answerRevealed1)}>{answerRevealed1 ? 1: set.a1}</Button></td>
+                    <td><Button onClick={() => reveal5(!answerRevealed5)}>{answerRevealed5 ? 5: set.a5}</Button></td>
                 </tr>
                 <tr>
-                    <td><Button onClick={() => reveal(!answerRevealed)}>{answerRevealed ? 2: set.a2}</Button></td>
-                    <td><Button onClick={() => reveal(!answerRevealed)}>{answerRevealed ? 6: set.a6}</Button></td>
+                    <td><Button onClick={() => reveal2(!answerRevealed2)}>{answerRevealed2 ? 2: set.a2}</Button></td>
+                    <td><Button onClick={() => reveal6(!answerRevealed6)}>{answerRevealed6 ? 6: set.a6}</Button></td>
                 </tr>
                 <tr>
-                    <td><Button onClick={() => reveal(!answerRevealed)}>{answerRevealed ? 3: set.a3}</Button></td>
-                    <td><Button onClick={() => reveal(!answerRevealed)}>{answerRevealed ? 7: set.a7}</Button></td>
+                    <td><Button onClick={() => reveal3(!answerRevealed3)}>{answerRevealed3 ? 3: set.a3}</Button></td>
+                    <td><Button onClick={() => reveal7(!answerRevealed7)}>{answerRevealed7 ? 7: set.a7}</Button></td>
                 </tr>
                 <tr>
-                    <td><Button onClick={() => reveal(!answerRevealed)}>{answerRevealed ? 4: set.a4}</Button></td>
-                    <td><Button onClick={() => reveal(!answerRevealed)}>{answerRevealed ? 8: set.a8}</Button></td>
+                    <td><Button onClick={() => reveal4(!answerRevealed4)}>{answerRevealed4 ? 4: set.a4}</Button></td>
+                    <td><Button onClick={() => reveal8(!answerRevealed8)}>{answerRevealed8 ? 8: set.a8}</Button></td>
                 </tr>
             </tbody>
         </Table>
